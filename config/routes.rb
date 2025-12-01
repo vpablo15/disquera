1f395fe2c2d853b48725bf354ab45532ba96dd71
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-12-01 16:51:01 -0300

    Add: Se agrega creación de usuarios por medio de otro usuario - Ya no se inicia sesión automáticamente al crear cuenta - Se modifica el lugar de redirección - Se eliminan control de bloqueo por autenticación al registro

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   namespace :admin do
     root "admin#home"
     resources :users, only: [:index, :destroy, :update, :edit]
   end
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
