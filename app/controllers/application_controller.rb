fcb55cd319281d6781aae1b55e0f374f77e93ca2
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-12-02 15:23:02 -0300

    Add: Se agrega redirección en el caso de no tener permisos y se agrega mensaje en español

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Este método atrapa la excepción CanCan::AccessDenied
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = exception.message || "No estás autorizado para realizar esta acción."
+    redirect_back(fallback_location: root_url, alert: exception.message)
+  end
 end
