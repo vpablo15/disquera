8cc87d42f9372457ee62ced69a5879d5b30fa6ef
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-12-02 16:05:27 -0300

    Add: Se protege control de registro para permitir crear solo a los administradores

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,6 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   skip_before_action :require_no_authentication, only: [:new, :create]
+  authorize_resource class: 'User', only: [:new, :create]
 
   # GET /resource/sign_up
   # def new
