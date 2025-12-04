0937116fc26528ea210ecad247dd6117ad8c067f
Tomás Campanella <tomyecamp@gmail.com>
2025-12-03 23:25:41 -0300

    Agrego relacionados}

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,7 +5,7 @@ class Album < ApplicationRecord
   has_many :images, dependent: :destroy # Las imágenes se borran si el disco se borra
   has_one :audio, dependent: :destroy # El audio se borra si el disco se borra
   has_many :sale_items
-  
+
   # Atributo: Tipo (Vinilo o CD)
   enum :media_type, {
     vinyl: "Vinilo",
@@ -19,4 +19,16 @@ class Album < ApplicationRecord
 
   # Scope para Soft Delete
   default_scope { where(deleted_at: nil) }
+
+  def related_albums
+    # Definir la consulta base para el Autor
+    by_author = Album.where(author_id: self.author_id)
+                     .where.not(id: self.id)
+    # Definir la consulta base para el Género
+    by_genre = Album.where(genre_id: self.genre_id)
+                    .where.not(id: self.id)
+
+    # Combinar ambas consultas usando OR y asegurar resultados únicos (distinct)
+    by_author.or(by_genre).distinct.limit(3)
+  end
 end
