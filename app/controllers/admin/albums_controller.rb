938af507eb85d9c2179635fa69b1f033cbbbc8cd
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-12-05 12:03:51 -0300

    Add: Se agrega inputs para agregar imágenes y archivos

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -14,7 +14,10 @@ class Admin::AlbumsController < ApplicationController
 
   # GET /albums/new
   def new
+    # 1. Inicializa el Album
     @album = Album.new
+    @album.images.build(is_cover: true, title: "Portada del Álbum")
+    @album.build_audio
   end
 
   # GET /albums/1/edit
@@ -23,29 +26,47 @@ class Admin::AlbumsController < ApplicationController
 
   # POST /albums or /albums.json
   def create
-    @album = Album.new(album_params)
 
-    respond_to do |format|
-      if @album.save
-        format.html { redirect_to admin_album_path(@album), notice: "Album was successfully created." }
-        format.json { render :show, status: :created, location: @album }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
+    Rails.logger.debug "Album Params Recibidos: #{album_params.inspect}"
+    @album = Album.new(album_params.except(:photo, :clip))
+    cover_file = album_params[:photo]
+    audio_file = album_params[:clip]
+
+    if @album.save
+      if cover_file.present?
+        @album.images.create!(
+          title: "#{@album.name} Cover",
+          is_cover: cover_file[:is_cover],
+        ) do |image|
+          # Aquí pasamos el objeto UploadedFile
+          image.photo.attach(cover_file[:photo])
+        end
+        flash[:notice] = "Portada adjuntada exitosamente."
       end
+      # --- B. Adjuntar Audio Único ---
+      if audio_file.present?
+        @album.create_audio! do |audio| audio.clip.attach(audio_file[:clip])
+        end
+        flash[:notice] = (flash[:notice] || "") + " Audio adjuntado exitosamente."
+      end
+      redirect_to @album, notice: 'Álbum creado exitosamente.'
+    else # Si el álbum no se guarda (ej. por validaciones de `name`), renderiza el formulario
+      render :new, status: :unprocessable_entity
     end
+  rescue StandardError => e
+    # Captura errores en la subida o creación
+    flash.now[:alert] = "Error al procesar los archivos: #{e.message}"
+    render :new, status: :unprocessable_entity
   end
 
   # PATCH/PUT /albums/1 or /albums/1.json
   def update
-    respond_to do |format|
-      if @album.update(album_params)
-        format.html { redirect_to admin_album_path(@album), notice: "Album was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @album }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
+    respond_to do |format| if @album.update(album_params)
+      format.html { redirect_to admin_album_path(@album), notice: "Album was successfully updated.", status: :see_other }
+      format.json { render :show, status: :ok, location: @album }
+    else format.html { render :edit, status: :unprocessable_entity }
+    format.json { render json: @album.errors, status: :unprocessable_entity }
+    end
     end
   end
 
@@ -59,25 +80,43 @@ correctamente habilitado/deshabilitado."
   def destroy
     @album.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to admin_albums_path, notice: "Album was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
+    respond_to do |format| format.html { redirect_to admin_albums_path, notice: "Album was successfully destroyed.", status: :see_other }
+    format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_album
-      @album = Album.unscope(where: :deleted_at).find(params[:id])
-    end
 
-    def set_album_context
-      @authors = Author.all
-      @genres = Genre.all
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_album
+    @album = Album.unscope(where: :deleted_at).find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def album_params
-      params.expect(album: [ :name, :description, :unit_price, :stock_available, :genre_id, :year, :media_type, :condition_is_new, :author_id, :deleted_at ])
-    end
+  def set_album_context
+    @authors = Author.all
+    @genres = Genre.all
+  end
+
+  # Only allow a list of trusted parameters through.
+  def album_params
+    params.require(:album).permit(
+      :name,
+      :description,
+      :unit_price,
+      :stock_available,
+      :genre_id,
+      :year,
+      :media_type,
+      :condition_is_new,
+      :author_id,
+      :deleted_at,
+
+      # Permite el hash anidado 'photo' y los campos permitidos dentro de él
+      photo: [:photo, :is_cover, :title],
+
+      # Permite el hash anidado 'clip' y el campo permitido dentro de él
+      clip: [:clip]
+    )
+  end
+
 end
