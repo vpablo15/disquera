31d43b517294fef5820a5e0075991bdd946bb411
Tomás Campanella <tomyecamp@gmail.com>
2025-12-07 20:58:01 -0300

    Arreglo estilos front

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Root
-  root "home#index"
+  root "albums#index"
 
   # PWA (comentado)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
