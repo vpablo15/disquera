0200a424f10e52236c40f03407d5f87261fab5b4
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-12-06 22:08:08 -0300

    Mod: Se modifica semilla de usuarios por datos mas simples

--- db/seeds/user_seed.rb
+++ db/seeds/user_seed.rb
@@ -18,10 +18,26 @@ manager_user = User.find_or_create_by!(email: MANAGER_EMAIL) do |user|
   user.role = User.roles[:manager]
 end
 
-NUM_TEST_USERS.times do |i|
-  email = "test_user_#{i + 1}@prueba.com"
+employee = User.find_or_create_by!(email: 'employee@employee') do |user|
+  user.password = COMMON_PASSWORD
+  user.password_confirmation = COMMON_PASSWORD
+  user.role = User.roles[:employee]
+end
+
+names = %w[lautaro pablo tomas]
+names.each do |i|
+  email = "#{i}@#{i}"
   User.find_or_create_by!(email: email) do |user|
     user.password = COMMON_PASSWORD
     user.password_confirmation = COMMON_PASSWORD
+    user.role = User.roles[:employee]
   end
 end
+
+# NUM_TEST_USERS.times do |i|
+#   email = "test_user_#{i + 1}@prueba.com"
+#   User.find_or_create_by!(email: email) do |user|
+#     user.password = COMMON_PASSWORD
+#     user.password_confirmation = COMMON_PASSWORD
+#   end
+# end
